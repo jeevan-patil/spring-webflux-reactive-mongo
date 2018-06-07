dd933ff5075874c4e1b59abc8c3862b950f637c3
Jeevan Patil <jeevanpaatil@gmail.com>
2018-06-07 19:39:21 +0530

    pushing the skeleton

---
+++ src/main/java/xyz/jeevan/springwebfluxreactivemongo/SpringWebfluxReactiveMongoApplication.java
@@ -0,0 +1,12 @@
+package xyz.jeevan.springwebfluxreactivemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxReactiveMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxReactiveMongoApplication.class, args);
+	}
+}
---
+++ src/test/java/xyz/jeevan/springwebfluxreactivemongo/SpringWebfluxReactiveMongoApplicationTests.java
@@ -0,0 +1,16 @@
+package xyz.jeevan.springwebfluxreactivemongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringWebfluxReactiveMongoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
